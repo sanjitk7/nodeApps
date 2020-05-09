8a74a156c81af10164b9ae9dfb0f6ceb519079a1
Sanjit Kumar <sanjitk2018@gmail.com>
2020-05-09 13:13:36 +0530

    final_working_commit

--- webServer/public/js/client.js
+++ webServer/public/js/client.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener("submit", (e) => {
 
 
     const location = search.value
-    url = "http://localhost:3000/weather?address=" + location
+    url = "/weather?address=" + location
     fetch(url).then((response) => {
  
         response.json().then((weatherData) => {
--- webServer/src/app.js
+++ webServer/src/app.js
@@ -6,6 +6,7 @@ const express = require("express")
 const hbs = require("hbs")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //set up paths
 const pathPublic = path.join(__dirname,"../public")
@@ -88,6 +89,6 @@ app.get("*", (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000")
+app.listen(port, () => {
+    console.log("Server listening on port ",port)
 })
