1ab0a3a5b60922dc4d3448daf071575b8f91b38f
Sanjit Kumar <sanjitk2018@gmail.com>
2020-07-11 17:03:19 +0530

    profanity and ack

--- chatApp/public/js/chat.js
+++ chatApp/public/js/chat.js
@@ -4,28 +4,46 @@ socket.on("message",(message)=> {
     console.log(message)
 })
 
-submitBtn = document.querySelector("#sendMessage")
-messageForm = document.querySelector("#message-form")
+$messageForm = document.querySelector("#message-form")
+$inputForm = $messageForm.querySelector("input")
+$submitBtn = $messageForm.querySelector("#sendMessage")
+$locationBtn = document.querySelector("#sendLocation")
 
-messageForm.addEventListener("submit", (e)=>{
+
+$messageForm.addEventListener("submit", (e)=>{
     e.preventDefault()
+    $submitBtn.setAttribute("disabled","disabled")
 
     message = e.target.elements.message.value
-    socket.emit("sendMessage",message)
+    socket.emit("sendMessage",message, (error)=>{
+        $submitBtn.removeAttribute("disabled")
+        $inputForm.value = ""
+        $inputForm.focus()
+
+        if (error){
+            return console.log(error)
+        }
+        console.log("Message delivered!")
+    })
 })
 
-locationBtn = document.querySelector("#sendLocation")
-locationBtn.addEventListener("click",()=>{
+
+$locationBtn.addEventListener("click",()=>{
+
     if (!navigator.geolocation){
         return alert("Location is not supported by your browser!")
     }
+    $locationBtn.setAttribute("diabled","disabled")
 
     navigator.geolocation.getCurrentPosition((position)=>{
         const locationObject = {
             lattitude:position.coords.latitude,
             longitude:position.coords.longitude
         }
-
-        socket.emit("sendLocation",locationObject)
+        
+        socket.emit("sendLocation",locationObject, ()=>{
+            console.log("Location shared!")
+            $locationBtn.removeAttribute("disabled")
+        })
     })
 })
--- chatApp/src/index.js
+++ chatApp/src/index.js
@@ -2,6 +2,7 @@ const http = require("http")
 const express = require("express")
 const path = require("path")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
 
 const app = express()
 const server = http.createServer(app)
@@ -23,12 +24,21 @@ io.on("connection", (socket)=>{
     socket.emit("message","Welcome!")
     socket.broadcast.emit("message","A new user has joined!")
 
-    socket.on("sendMessage", (data)=> {
+    socket.on("sendMessage", (data, callback)=> {
+        
+        const filter = new Filter()
+        if (filter.isProfane(data)){
+            return callback("Profanity is not allowed!")
+        }
+        
         io.emit("message",data)
+
+        callback()
     })
 
-    socket.on("sendLocation", (position)=>{
+    socket.on("sendLocation", (position,callback)=>{
         io.emit("message",`https://www.google.com/maps?q=${position.lattitude},${position.longitude}`)
+        callback("sent")
     })
 
     socket.on("disconnect",()=>{
